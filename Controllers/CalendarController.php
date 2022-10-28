2dade03f74ff43da71e784697c58224aa49b0ce2
daaviid29 <daaviidpuga29@gmail.com>
2022-10-28 19:26:07 +0200

    Comentarios CalendarController

--- Controllers/CalendarController.php
+++ Controllers/CalendarController.php
@@ -1,9 +1,11 @@
 <?php
 
+    // Cargamos el fichero view donde construiremos las vistas (añadiremos todos los includes para formar la vista de forma anónima para el usuario)
     require_once "Views/view.php";
 
     class CalendarController{
 
+        // Método para mostrar el Calendario
         public function mostrarCalendario(){
             // Requerimos la vista donde mostraremos el contenido
             View::adminViews('admin-calendar');
